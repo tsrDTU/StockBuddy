eda7c94fc5e176cb9392738db329c5482e58fd4c
tsrDTU <s047560@dtu.dk>
2024-01-03 17:42:30 +0100

    Data upload til Firestore virker

--- app/src/main/java/com/stockbuddy/Test.kt
+++ app/src/main/java/com/stockbuddy/Test.kt
@@ -1,11 +1,62 @@
 package com.stockbuddy
 
+import androidx.compose.material3.Surface
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+import androidx.compose.foundation.layout.fillMaxSize
+
+import androidx.compose.material3.MaterialTheme
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.stockbuddy.domain.users.addUser
 import com.stockbuddy.domain.users.readUser
 
-class Test {
-    fun Test() {
-        addUser("tsr")
+
+class Test: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+
+
+
+
+
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+        addUser("tsr", "Torben", "Rasmussen","tsr@dtu")
         readUser("tsr")
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        TestTheme {
+
+            Greeting("Android")
+        }
     }
 }
--- app/src/main/java/com/stockbuddy/domain/users/UserInformation.kt
+++ app/src/main/java/com/stockbuddy/domain/users/UserInformation.kt
@@ -2,15 +2,25 @@ package com.stockbuddy.domain.users
 
 import android.content.ContentValues.TAG
 import android.util.Log
+import android.widget.TextView
 import com.google.firebase.Firebase
-import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.firestore
 
-val db = Firebase.firestore
 
-fun addUser (userId : String) {
+
+
+
+fun addUser (userId : String, first : String, last : String, email : String) {
+    val user = hashMapOf<String, String>(
+       "UserId" to userId,
+        "FirstName" to first,
+        "LastName" to last,
+        "EmailAddress" to email
+
+    )
+    val db = Firebase.firestore
     db.collection("users")
-        .add(userId)
+        .add(user)
         .addOnSuccessListener {documentReference ->
           Log.d (TAG,  "DocumentSnapshot added with ID: $(documentReference.id)")
         }
@@ -20,15 +30,20 @@ fun addUser (userId : String) {
 }
 
 fun readUser(userId: String){
-    db.collection("users.userId")
+    val textView: TextView? = null
+    val db = Firebase.firestore
+  val usr = db.collection("users.userId")
         .get()
         .addOnSuccessListener { result ->
             for (document in result){
                 Log.d(TAG, "${document.id} => ${document.data}")
+ //               textView?.text = "${document.id}=> ${document.data}  "
             }
 
         }
         .addOnFailureListener { exception ->
             Log.d(TAG, "Error adding document", exception)
         }
+    textView?.text = usr.toString()
+
 }
