c8601abb35f1e6606d4c31ad44e126f141e01256
tsrDTU <s047560@dtu.dk>
2024-01-06 06:40:56 +0100

    Data læses fra Firestore ind i Composable forbedret

--- app/src/main/java/com/stockbuddy/Test.kt
+++ app/src/main/java/com/stockbuddy/Test.kt
@@ -1,38 +1,22 @@
 package com.stockbuddy
 
-import android.R
-import android.content.DialogInterface
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import com.stockbuddy.domain.users.MyViewModel
-import com.stockbuddy.domain.users.ReadUser
-import com.stockbuddy.domain.users.getFirstName
-import com.stockbuddy.domain.users.getLastName
-import com.stockbuddy.domain.users.getNytTal
+import com.stockbuddy.domain.users.selectUserInFirestore
 
-//import res.drawable.img_notification.xml
 
 
 
@@ -48,68 +32,34 @@ class Test: ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 
-                    MyScreen(viewModel = MyViewModel())
-                    Greeting("Android")
+                    selectUserInFirestore("tsr")
+                    ShowUserInformation(viewModel = MyViewModel())
+
                 }
             }
         }
 
     }
 /*
-From ChatGPT
+With assistance from ChatGPT
 
  */
 
     @Composable
-    fun MyScreen(viewModel: MyViewModel) {
+    fun ShowUserInformation(viewModel: MyViewModel) {
         val dataList by viewModel.actUser.collectAsState()
 
-
-        // Brug dataList til at bygge din komposable
         LazyColumn {
             items(dataList) { dataList ->
-                // Din ListItemComposable f.eks.
-                Text(text = dataList.toString())
+                // ListItemComposable
+   //             Text(text = dataList.toString())
+                Text(text = dataList.FirstName.toString())
+                Text(text = dataList.LastName.toString())
             }
         }
     }
 
-
-
-
-
-
-
-    @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
-    var n : Int = 1
-        var str : String
-
-
-
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-
-
-
-    }
-
-
-
-    @Preview(showBackground = true)
-    @Composable
-    fun GreetingPreview() {
-        TestTheme {
-
-            Greeting("Android")
-        }
-    }
-}
-/*
-private fun <RowScope> RowScope.Text(println: Unit) {
-
 }
 
- */
+
+
--- app/src/main/java/com/stockbuddy/domain/users/UserInformation.kt
+++ app/src/main/java/com/stockbuddy/domain/users/UserInformation.kt
@@ -1,28 +1,15 @@
 package com.stockbuddy.domain.users
 
-//noinspection SuspiciousImport
-
 import android.content.ContentValues.TAG
 import android.util.Log
-import android.widget.TextView
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
-import com.google.firebase.firestore.toObject
 import com.stockbuddy.data.UserData
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
-import java.lang.Thread.sleep
-import kotlinx.coroutines.*
-import kotlinx.coroutines.android.awaitFrame
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-private var FirstName : String = " XY "
-private var LarstName : String = " AB "
+private var userIdFirestore = ""
 
 fun addUser (userId : String, first : String, last : String, email : String) {
     val user = hashMapOf<String, String>(
@@ -43,83 +30,10 @@ fun addUser (userId : String, first : String, last : String, email : String) {
         }
 }
 
-
- fun ReadUser(userId: String){
-
-    val db = Firebase.firestore
-    var fieldValue : String = ""
-
-
-   db.collection("users")
-        .whereEqualTo("UserId", userId)
-        .get()
-        .addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                for (document in task.result) {
-                    Log.d(TAG, document.id + " => " + document.data)
-                    // You can now work with the document
-                    // For example, to get a field's value:
-                     fieldValue = document.getString("FirstName").toString()
-                }
-            } else {
-                Log.d(TAG, "Error getting documents: ", task.exception)
-            }
-        }
-
+fun selectUserInFirestore(userId: String){
+    userIdFirestore = userId
 }
 
-
-
-fun  ReadUserOnly(userId: String, actUser: List<UserData>): String= runBlocking{
-    val db = Firebase.firestore
-
-    val textView: TextView? = null
-
-    val job = launch {
-        db.collection("users")
-            .whereEqualTo("UserId", userId)
-            .get()
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    for (document in task.result) {
-                        Log.d(TAG, document.id + " => " + document.data)
-                        // You can now work with the document
-                        // For example, to get a field's value:
-                        FirstName = document.getString("FirstName").toString()
-                        actUser
-
-                        textView?.setText(FirstName)
-
-
-
-
-                    }
-                } else {
-                    Log.d(TAG, "Error getting documents: ", task.exception)
-                }
-            }
-    }
-
-    job.join() // wait until child coroutine completes
-
-    return@runBlocking FirstName
-}
-
-fun getFirstName(): String {
-
-    return FirstName
-}
-
-
-fun getLastName(): String {
-
-    return LarstName
-}
-
-fun getNytTal(n : Int) : Int {
-
-    return n + 1
-}
 
 /*
 With assistance from chatGPT
@@ -129,7 +43,7 @@ class MyViewModel : ViewModel() {
     var actUser: StateFlow<List<UserData>> = _actUser
 
     init {
-        ReadUser("tsr")
+        ReadUser(userIdFirestore)
     }
 
   private fun ReadUser(userId: String){
@@ -174,5 +88,3 @@ class MyViewModel : ViewModel() {
 
     }
 }
-
-
