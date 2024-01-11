e4c026a96ebe719f62e0383039d9ce9778ad7114
tsrDTU <s047560@dtu.dk>
2024-01-11 15:04:43 +0100

    Fejl rettet. Formatteret udskrift. addStock ændret til purchaseStock

--- app/src/main/java/com/stockbuddy/Test.kt
+++ app/src/main/java/com/stockbuddy/Test.kt
@@ -7,17 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.stockbuddy.domain.users.ShowStockInformation
 import com.stockbuddy.domain.users.ShowTotalStockValue
-import com.stockbuddy.domain.users.UserViewModel
-import com.stockbuddy.domain.users.StockViewModel
-import com.stockbuddy.domain.users.ShowUserInformation
-import com.stockbuddy.domain.users.StockTotalVavueViewModel
-import com.stockbuddy.domain.users.addStock
-import com.stockbuddy.domain.users.addUser
-import com.stockbuddy.domain.users.selectStockInFirestore
+import com.stockbuddy.domain.users.StockTotalValueViewModel
 import com.stockbuddy.domain.users.selectUserInFirestore
-import com.stockbuddy.domain.users.sellStock
 
 
 class Test: ComponentActivity() {
@@ -36,15 +28,17 @@ class Test: ComponentActivity() {
                //     addUser("tt", "Tin", "Tin", "tin.tin@mail.belg")
               //      addUser("kh", "Kaptajn", "Haddock", "kaptajn.haddock@mail.belg")
                //     addUser("ll", "Lucky", "Luke", "lucky.luke@mail.detvildevesten")
-          //          addStock("bd", "IBM", 3447,315.0,12.0, "2024-01-06" )
-          //          addStock("bd", "HP", 2475,733.0,15.0, "2024-01-08" )
-          //          addStock("bd", "MicroSoft", 987,876.0,18.0, "2024-01-10" )
+          //          purchaseStock("bd", "IBM", 3447,315.0,12.0, "2024-01-06" )
+          //          purchaseStock("bd", "HP", 2475,733.0,15.0, "2024-01-08" )
+          //          purchaseStock("bd", "MicroSoft", 987,876.0,18.0, "2024-01-10" )
+          //          purchaseStock("bd", "Google", 667,1340.0,18.0, "2024-01-11" )
           //          sellStock ("bd", "IBM", 547.0, 18.0, "2024-01-10")
+         //           sellStock ("bd", "HP", 2814.0, 18.0, "2024-01-11")
                     selectUserInFirestore("bd")
           //          selectStockInFirestore("IBM")
            //         ShowUserInformation(viewModel = UserViewModel())
-           //         ShowStockInformation(viewModel = StockViewModel())
-                    ShowTotalStockValue(viewModel = StockTotalVavueViewModel())
+            //        ShowStockInformation(viewModel = StockViewModel())
+                    ShowTotalStockValue(viewModel = StockTotalValueViewModel())
 
                 }
             }
--- app/src/main/java/com/stockbuddy/domain/users/StockInformation.kt
+++ app/src/main/java/com/stockbuddy/domain/users/StockInformation.kt
@@ -2,7 +2,6 @@ package com.stockbuddy.domain.users
 
 
 import android.content.ContentValues.TAG
-import android.os.Environment
 import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
@@ -15,18 +14,11 @@ import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
 import com.stockbuddy.data.StockData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.stockbuddy.R
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 //import kotlinx.coroutines.flow.internal.NoOpContinuation.context
-import java.io.BufferedWriter
-import java.io.File
-import java.io.FileWriter
-import java.io.IOException
-import java.io.InputStream
+
 //import kotlin.coroutines.jvm.internal.CompletedContinuation.context
 
 //private var userIdFirestore = ""
@@ -35,7 +27,7 @@ public var stockNameFirestore = ""
 
 //val inputStream: InputStream = context.resources.openRawResource(R.raw.my_file)
 
-fun addStock (userId: String, stockName: String, numOfStocks : Int, purPrice: Double, purCost: Double, purDate: String) {
+fun purchaseStock (userId: String, stockName: String, numOfStocks : Int, purPrice: Double, purCost: Double, purDate: String) {
    val stock = hashMapOf(
         "UserId" to userId,
         "StockName" to stockName,
@@ -70,36 +62,51 @@ fun selectStockInFirestore(stockName: String){
 }
 
 
-/* created by ChatGPT modified by Torben */
+/* ChatGPT has given an example of how it is possible to update a field. Then the code is modified by Torben */
 fun sellStock (userId: String, stockName: String,sellPriceEuro: Double, sellCostEuro: Double, sellDate: String) {
 
 
     val db = Firebase.firestore
     val collectionRef = db.collection("stockTradingHistory")
     val query = collectionRef.whereEqualTo("UserId", userId).whereEqualTo("StockName", stockName)
+    var alreadySold : Boolean = false
 
-    query.get()
-        .addOnSuccessListener { documents ->
-            for (document in documents) {
-                val docRef = collectionRef.document(document.id)
-                val updatedData = hashMapOf(
-                    "Sold" to true,
-                    "SellPriceEuro" to sellPriceEuro,
-                    "SellCostEuro" to sellCostEuro,
-                    "SellDate" to sellDate
-                )
-                docRef.update(updatedData as Map<String, Any>)
-                    .addOnSuccessListener {
-                        Log.d("StateFlow", "Document updated successfully!")
-                    }
-                    .addOnFailureListener { e ->
-                        Log.w("StateFlow", "Error in updating document", e)
-                    }
+    query.get().addOnCompleteListener { task ->
+        if (task.isSuccessful) {
+
+            for (document in task.result) {
+                if (document.getBoolean("Sold") == true) {
+                    alreadySold = true
+                }
+
             }
         }
-        .addOnFailureListener { exception ->
-            Log.w("StateFlow", "Error in searching for document", exception)
-        }
+    }
+
+    if (!alreadySold) {
+        query.get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    val docRef = collectionRef.document(document.id)
+                    val updatedData = hashMapOf(
+                        "Sold" to true,
+                        "SellPriceEuro" to sellPriceEuro,
+                        "SellCostEuro" to sellCostEuro,
+                        "SellDate" to sellDate
+                    )
+                    docRef.update(updatedData as Map<String, Any>)
+                        .addOnSuccessListener {
+                            Log.d("StateFlow", "Document updated successfully!")
+                        }
+                        .addOnFailureListener { e ->
+                            Log.w("StateFlow", "Error in updating document", e)
+                        }
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("StateFlow", "Error in searching for document", exception)
+            }
+    }
 }
 
 
@@ -191,7 +198,7 @@ fun ShowStockInformation(viewModel: StockViewModel) {
 
 
 
-class StockTotalVavueViewModel : ViewModel() {
+class StockTotalValueViewModel : ViewModel() {
     private var _actStock = MutableStateFlow<List<StockData>>(emptyList())
     var actStock: StateFlow<List<StockData>> = _actStock
 
@@ -200,6 +207,10 @@ class StockTotalVavueViewModel : ViewModel() {
 
     private val _totalProfit = MutableStateFlow<Double>(0.0)
     val totalProfit: StateFlow<Double> = _totalProfit.asStateFlow()
+
+    private val _totalProfitInPct = MutableStateFlow<Double>(0.0)
+    val totalProfitInPct: StateFlow<Double> = _totalProfitInPct.asStateFlow()
+
 
 
 
@@ -211,6 +222,8 @@ class StockTotalVavueViewModel : ViewModel() {
 
         val db = Firebase.firestore
         var fieldValue : String = ""
+        var totalProf : Double = 0.0
+        var totalValue : Double = 0.0
 
         db.collection("stockTradingHistory")
             .whereEqualTo("UserId", userId)
@@ -252,8 +265,8 @@ class StockTotalVavueViewModel : ViewModel() {
                             val purp = usr.PurPriceEuro
                             val purc = usr.PurCostEuro
 
-                            _totalStockValue.value +=
-                                nbs!! * purp!! - purc!!
+                            totalValue += nbs!! * purp!! - purc!!
+                            Log.d("StateFlow", " Total Value $totalValue")
                         }
                         else
                         {
@@ -263,11 +276,17 @@ class StockTotalVavueViewModel : ViewModel() {
                             val selp = usr.SellPriceEuro
                             val selc = usr.SellCostEuro
 
-                            _totalProfit.value += nbs!! * (selp!! - purp!!) - (purc!! + selc!!)
-                                Log.d("StateFlow", "$_totalProfit")
+                          //  _totalProfit.value
+                            totalProf += nbs!! * (selp!! - purp!!) - (purc!! + selc!!)
+                            Log.d("StateFlow", " Total Profit $totalProf")
                         }
 
                     }
+
+                    _totalStockValue.value += totalValue
+                    _totalProfit.value += totalProf
+                    _totalProfitInPct.value += 100.0 * totalProf/totalValue
+
                 }
             }
     }
@@ -279,13 +298,14 @@ private operator fun <T> MutableStateFlow<T>.plusAssign(t: T) {
 
 
 @Composable
-fun ShowTotalStockValue(viewModel: StockTotalVavueViewModel) {
+fun ShowTotalStockValue(viewModel: StockTotalValueViewModel) {
     val totalStockValue by viewModel.totalStockValue.collectAsState()
     val totalProfit by viewModel.totalProfit.collectAsState()
+    val totalProfitInPct by viewModel.totalProfitInPct.collectAsState()
 
     Column {
         Text(text = "Total Value of Stocks: $totalStockValue Euro")
-        Text(text = "Total profit: $totalProfit Euro")
+        Text(text = String.format("Total profit: $totalProfit Euro, %.2f pct.", totalProfitInPct))
     }
 }
 
